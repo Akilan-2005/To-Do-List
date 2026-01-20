cd5cfa5afc17285db8662ff371d0454a89346072
Akilan-2005 <akilandk2005@gmail.com>
2026-01-20 12:59:46 +0530

    first commit

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+
+
+
+function Footer(){
+    return(
+        <>
+        
+            <p className="foot"> All rights reserved &copy; 2026 Akilan</p>
+        </>
+
+    )
+}
+
+export default Footer;
